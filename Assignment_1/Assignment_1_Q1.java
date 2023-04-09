f1cf39ada031be8e96be8ad6439053bd2fea4cfb
Aditya Sahu <118821421+god-father-99@users.noreply.github.com>
2023-04-09 19:03:02 +0530

    Create Assignment_1_Q1.java

---
+++ Assignment_1/Assignment_1_Q1.java
@@ -0,0 +1,26 @@
+package ITER.Sem2;
+import java.util.Scanner;
+/*
+  Created by Aditya Sahu
+  Regd. no.-2241019168
+  sec-22441012
+*/
+public class Assignment_1_Q1
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter positive integer greater than 2 : ");
+		int a=sc.nextInt();
+		int count=0;
+		while(a>2)
+		{
+			count++;
+			a=a/2;
+		}
+		System.out.println("The number of times one must repeatedly divide this number"
+							+"by 2 before getting a value less than 2 is : "+count);
+	}
+
+}
